ae3e7b51e7e8d983d298ba8284123447d4d84309
devangj <devang.j1998@gmail.com>
2018-06-12 18:15:52 +0530

    migrated to postgresql

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template, flash, redirect, url_for, abort, session, request
 from flask_bcrypt import check_password_hash
-from datetime import datetime
 import forms
-from db import *
+import db
 from flask_wtf.csrf import CSRFProtect
-
 DEBUG = True
 PORT = 8000
 HOST = '0.0.0.0'
@@ -14,32 +12,35 @@ app = Flask(__name__)
 app.secret_key = 'thisisareallylongkeythatnooneknows'
 csrf = CSRFProtect(app)
 
-#Home Page
+
+# Home Page
 @app.route('/')
 def index():
     if session.get('login'):
-        user = members_list()
-        stream = post_list()[:100]
-        return render_template('stream.html', stream = stream, user=user)
+        user = db.members_list()
+        stream = db.post_list()[:100]
+        return render_template('stream.html', stream=stream, user=user)
     else:
         return redirect(url_for('login'))
+
 
 # Register User
 @app.route('/register', methods=('GET', 'POST'))
 def register():
     form = forms.RegisterForm()
     if form.validate_on_submit():
-        register_user(form.username.data, form.password.data, form.email.data, form.dob.data, 'Tell us a bit about yourself')
+        db.register_user(form.username.data, form.password.data, form.email.data, form.dob.data, 'Tell us a bit about yourself')
         flash("Registration Successful", "success")
         return redirect(url_for('index'))
     return render_template('register.html', form=form)
 
-#Login User
+
+# Login User
 @app.route('/login', methods=('GET', 'POST'))
 def login():
-    form= forms.LoginForm()
+    form = forms.LoginForm()
     if form.validate_on_submit():
-        result = login_user(form.email.data, form.password.data)
+        result = db.login_user(form.email.data, form.password.data)
         if result:
             session['login'] = True
             session['user_id'] = result[0]
@@ -52,7 +53,8 @@ def login():
             flash("Your email or password doesn't match!", "error")
     return render_template('login.html', form=form)
 
-#Logout User
+
+# Logout User
 @app.route('/logout')
 def logout():
     if session['login']:
@@ -64,197 +66,221 @@ def logout():
         flash("You've been logged out! Come back soon!", "success")
         return redirect(url_for('index'))
 
-#Make New Post
-@app.route('/new_post', methods = ('GET', 'POST'))
+
+# Make New Post
+@app.route('/new_post', methods=('GET', 'POST'))
 def post():
     if session['login']:
         form = forms.PostForm()
         if form.validate_on_submit():
-            add_post(user_id = str(session['user_id']), article = form.content.data.strip())
+            db.add_post(user_id=str(session['user_id']), article=form.content.data.strip())
             flash('Your Message has been posted!', 'success')
             return redirect(url_for('index'))
-        return render_template('post.html', form = form)
+        return render_template('post.html', form=form)
 
-#Delete Post
+
+# Delete Post
 @app.route('/delete/<post_id>')
 def delete_clicked_post(post_id):
     if session['login']:
-        result = delete_post(post_id)
+        db.delete_post(post_id)
         return redirect(request.referrer)
 
-#Edit Post
-@app.route('/edit/<int:post_id>', methods = ('GET', 'POST'))
+
+# Edit Post
+@app.route('/edit/<int:post_id>', methods=('GET', 'POST'))
 def edit_clicked_post(post_id):
     if session['login']:
-        form = forms.PostForm(content = post_list_by_post_id(post_id)[0][2])
+        form = forms.PostForm(content=db.post_list_by_post_id(post_id)[0][2])
         if form.validate_on_submit():
-            edit_post(post_id = post_id, article = form.content.data.strip())
+            db.edit_post(post_id=post_id, article=form.content.data.strip())
             flash('Your Post has been edited!', 'success')
             return redirect(url_for('index'))
-        return render_template('post.html', form = form)
+        return render_template('post.html', form=form)
 
-#Display Single Post
+
+# Display Single Post
 @app.route('/post/<int:post_id>')
 def view_post(post_id):
-    posts = post_list_by_post_id(post_id)
+    posts = db.post_list_by_post_id(post_id)
     if len(posts) == 100:
         abort(404)
-    return render_template('stream.html', stream = posts, use = 1)
+    return render_template('stream.html', stream=posts, use=1)
 
-#Displays Posts
+
+# Displays Posts
 @app.route('/stream')
 @app.route('/stream/<int:user_id>')
-def stream(user_id = None):
+def stream(user_id=None):
     template = 'stream.html'
     if user_id and user_id != session['user_id']:
-        user = member_info(str(user_id))
-        stream = post_list_by_my_id(user_id)[:100]
+        user = db.member_info(str(user_id))
+        stream = db.post_list_by_my_id(user_id)[:100]
     else:
-        user = member_info(str(session['user_id']))
-        stream = post_list_by_id(str(session['user_id']))[:100]
+        user = db.member_info(str(session['user_id']))
+        stream = db.post_list_by_id(str(session['user_id']))[:100]
     if user_id:
         template = 'user_stream.html'
-    return render_template(template, stream = stream, user = user)
+    return render_template(template, stream=stream, user=user)
 
-#Follow A User
+
+# Follow A User
 @app.route('/follow/<user_id>')
 def follow(user_id):
     if session['login']:
         try:
-            to_user = member_info(user_id)
+            to_user = db.member_info(user_id)
         except to_user is False:
             abort(404)
         else:
-            result = follow_user(session['user_id'], to_user[0][0])
+            db.follow_user(session['user_id'], to_user[0][0])
             flash("You are now following {}!".format(to_user[0][1]), 'success')
         return redirect(request.referrer)
 
-#Unfollow A User
+
+# Unfollow A User
 @app.route('/unfollow/<user_id>')
 def unfollow(user_id):
     if session['login']:
         try:
-            to_user = member_info(user_id)
+            to_user = db.member_info(user_id)
         except to_user is False:
             abort(404)
         else:
-            result = unfollow_user(session['user_id'], to_user[0][0])
+            db.unfollow_user(session['user_id'], to_user[0][0])
             flash("You unfollowed {}!".format(to_user[0][1]), 'success')
         return redirect(request.referrer)
 
-#Like Post
+
+# Like Post
 @app.route('/like/<post_id>')
 def like(post_id):
     if session['login']:
         try:
-            to_post = post_list_by_post_id(post_id)
+            to_post = db.post_list_by_post_id(post_id)
         except to_post is False:
             abort(404)
         else:
-            result = like_post(session['user_id'], to_post[0][0])
+            db.like_post(session['user_id'], to_post[0][0])
         return redirect(request.referrer)
 
-#Unlike Post
+
+# Unlike Post
 @app.route('/unlike/<post_id>')
 def unlike(post_id):
     if session['login']:
         if session['login']:
             try:
-                to_post = post_list_by_post_id(post_id)
+                to_post = db.post_list_by_post_id(post_id)
             except to_post is False:
                 abort(404)
             else:
-                result = unlike_post(session['user_id'], to_post[0][0])
+                db.unlike_post(session['user_id'], to_post[0][0])
             return redirect(request.referrer)
 
-#Show Profile
+
+# Show Profile
 @app.route('/profile/<user_id>')
 def profile(user_id):
-    user = member_info(user_id)
-    return render_template('profile.html', user = user)
+    user = db.member_info(user_id)
+    return render_template('profile.html', user=user)
 
-#Settings
+
+# Settings
 @app.route('/settings')
 def settings():
     return render_template('settings.html')
 
+
+# Edit Profile
 @app.route('/settings/profile_edit', methods=('GET', 'POST'))
 def settings_profile_edit():
     if session['login']:
-        user = member_info(session['user_id'])
-        form = forms.ProfileForm(x = user[0][0], username = user[0][1], email = user[0][3], dob = user[0][4], bio = user[0][5])
+        user = db.member_info(session['user_id'])
+        form = forms.ProfileForm(x=user[0][0], username=user[0][1], email=user[0][3], dob=user[0][4], bio=user[0][5])
         if form.validate_on_submit():
-            if (form.username.data in username_not_mine_fetch(session['username'])):
+            if (form.username.data in db.username_not_mine_fetch(session['username'])):
                 flash("Username is already in use!", "error")
-            elif (form.email.data in email_not_mine_fetch(session['email'])):
+            elif (form.email.data in db.email_not_mine_fetch(session['email'])):
                 flash("Email is already in use!", "error")
             else:
-                result = update_member(session['user_id'], form.username.data, form.email.data, form.dob.data, form.bio.data)
+                result = db.update_member(session['user_id'], form.username.data, form.email.data, form.dob.data, form.bio.data)
                 if result:
                     session['username'] = form.username.data
                     session['email'] = form.email.data
                     session['dob'] = form.dob.data
                     flash('Edited', 'success')
                     return redirect(url_for('settings'))
-        return render_template('profile_edit.html', form = form)
+        return render_template('profile_edit.html', form=form)
 
+
+# Set New Password
 @app.route('/settings/new_password', methods=('GET', 'POST'))
 def settings_new_password():
     if session['login']:
         form = forms.PasswordForm()
-        user = member_info(session['user_id'])
+        user = db.member_info(session['user_id'])
         if form.validate_on_submit():
             if check_password_hash(user[0][2], form.currentpassword.data):
-                update_password(user[0][0], form.newpassword.data)
+                db.update_password(user[0][0], form.newpassword.data)
                 flash('New password set', 'success')
                 return redirect(url_for('settings'))
             else:
                 flash("Incorrect Password!", "error")
-        return render_template('password_edit.html', form = form)
+        return render_template('password_edit.html', form=form)
 
+
+# Delete Account
 @app.route('/settings/delete_account', methods=('GET', 'POST'))
 def settings_delete_account():
     if session['login']:
         form = forms.DeleteForm()
-        user = member_info(session['user_id'])
+        user = db.member_info(session['user_id'])
         if form.validate_on_submit():
             if check_password_hash(user[0][2], form.password.data):
-                delete_member(user[0][0])
+                db.delete_member(user[0][0])
                 flash('Account Deleted', 'success')
                 logout()
                 return redirect(url_for('index'))
             else:
                 flash("Incorrect Password!", "error")
-        return render_template('delete_account.html', form = form)
+        return render_template('delete_account.html', form=form)
 
-#Display Members
+
+# Display Members
 @app.route('/stream_member')
 def stream_member():
-    stream = username_fetch()
-    return render_template('stream_members.html', stream = stream)
+    stream = db.username_fetch()
+    return render_template('stream_members.html', stream=stream)
 
+
+# Following List
 @app.route('/stream_member/following')
 def stream_member_following():
-    stream = following_list(session['user_id'])
-    return render_template('stream_members.html', stream = stream)
+    stream = db.following_list(session['user_id'])
+    return render_template('stream_members.html', stream=stream)
 
+
+# Follower List
 @app.route('/stream_member/follower')
 def stream_member_follower():
-    stream = follower_list(session['user_id'])
-    return render_template('stream_members.html', stream = stream)
+    stream = db.follower_list(session['user_id'])
+    return render_template('stream_members.html', stream=stream)
 
-#Handle 404 Errors
+
+# Handle 404 Errors
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
 
-#Main Function
+
+# Main Function
 if __name__ == '__main__':
-    app.jinja_env.globals.update(username_fetch_by_id=username_fetch_by_id)
-    app.jinja_env.globals.update(following_list=following_list)
-    app.jinja_env.globals.update(follower_list=follower_list)
-    app.jinja_env.globals.update(post_list_by_id=post_list_by_id)
-    app.jinja_env.globals.update(like_list_by_post_id=like_list_by_post_id)
-    app.jinja_env.globals.update(post_list_by_my_id=post_list_by_my_id)
-    app.jinja_env.globals.update(user_id_fetch_by_name=user_id_fetch_by_name)
+    app.jinja_env.globals.update(username_fetch_by_id=db.username_fetch_by_id)
+    app.jinja_env.globals.update(following_list=db.following_list)
+    app.jinja_env.globals.update(follower_list=db.follower_list)
+    app.jinja_env.globals.update(post_list_by_id=db.post_list_by_id)
+    app.jinja_env.globals.update(like_list_by_post_id=db.like_list_by_post_id)
+    app.jinja_env.globals.update(post_list_by_my_id=db.post_list_by_my_id)
+    app.jinja_env.globals.update(user_id_fetch_by_name=db.user_id_fetch_by_name)
     app.run(debug=DEBUG, host=HOST, port=PORT, threaded=THREADED)
--- db.py
+++ db.py
@@ -1,38 +1,54 @@
-import mysql.connector
-# import psycopg2
+# import mysql.connector
+import psycopg2
 from flask_bcrypt import generate_password_hash, check_password_hash
 
-#DB connections and calls
-def connectDB(host='localhost', database='thesocialnetwork', user='root', password='1234'):
-    return mysql.connector.connect(host=host, database=database, user=user, password=password)
 
+# DB connections and calls
+# Connect To Database
 # def connectDB(host='localhost', database='thesocialnetwork', user='root', password='1234'):
-#     return psycopg2.connect("host=%s, dbname=%s, user=%s, password=%s", (host, database, user, password))
+#     return mysql.connector.connect(host=host, database=database, user=user, password=password)
 
+def connectDB(host='localhost', database='thesocialnetwork', user='root', password='1234', port='5432'):
+    return psycopg2.connect("host="+host+" dbname="+database+" user="+user+" password="+password+" port="+port)
+
+
+# Disconnect From Database
 def disconnectDB(conn):
     conn.close()
 
+
+# Execute A Query
 def executeDB(conn, sql, values):
     cursor = conn.cursor()
     cursor.execute(sql, values)
     conn.commit()
+    return cursor.lastrowid
 
+
+# Query The Database
 def queryDB(conn, sql):
     cursor = conn.cursor()
     cursor.execute(sql)
-    rows = cursor.fetchall()
+    try:
+        rows = cursor.fetchall()
+    except:
+        rows = []
     cursor.close()
     return rows
 
 
-#Register and Login
+# Register and Login
+# Register User
 def register_user(username, password, email, dob, bio):
     c = connectDB()
     password = generate_password_hash(password)
-    executeDB(c, "insert into members values(0, %s, %s, %s, %s, %s)", (username, password, email, dob, bio))
+    password = str(password, "utf-8")
+    executeDB(c, "insert into members values(default, %s, %s, %s, %s, %s)", (username, password, email, dob, bio))
     disconnectDB(c)
     return True
 
+
+# Login User
 def login_user(email, password):
     c = connectDB()
     result = queryDB(c, "select * from members where email ='"+email+"'")
@@ -45,197 +61,243 @@ def login_user(email, password):
     else:
         return False
 
+
+# Update Member
 def update_member(user_id, username, email, dob, bio):
     c = connectDB()
     user_id = str(user_id)
-    executeDB(c,"update members set username=%s, email=%s, bio=%s, dob=%s where user_id=%s",(username, email, bio, dob, user_id))
+    executeDB(c, "update members set username=%s, email=%s, bio=%s, dob=%s where user_id=%s", (username, email, bio, dob, user_id))
     disconnectDB(c)
     return True
 
+
+# Update Password
 def update_password(user_id, password):
     c = connectDB()
     user_id = str(user_id)
     password = generate_password_hash(password)
-    executeDB(c,"update members set password=%s where user_id=%s",(password, user_id))
+    password = str(password, "utf-8")
+    executeDB(c, "update members set password=%s where user_id=%s", (password, user_id))
     disconnectDB(c)
     return True
 
+
+# Delete Member
 def delete_member(user_id):
     c = connectDB()
     user_id = str(user_id)
-    executeDB(c,"delete from members where user_id="+user_id,())
+    executeDB(c, "delete from members where user_id="+user_id, ())
     flush_follow(user_id)
     flush_posts(user_id)
     disconnectDB(c)
     return True
 
-#Posts Related
+
+# Posts Related
+# Add Post
 def add_post(user_id, article):
     c = connectDB()
     user_id = str(user_id)
-    executeDB(c,"insert into posts values(0,"+user_id+",%s, now())",(article,))
+    executeDB(c, "insert into posts values(default,"+user_id+",%s, now())", (article,))
     disconnectDB(c)
     return True
 
+
+# Delete Post
 def delete_post(post_id):
     c = connectDB()
     post_id = str(post_id)
-    executeDB(c,"delete from posts where post_id="+post_id,())
+    executeDB(c, "delete from posts where post_id="+post_id, ())
     flush_likes(post_id)
     disconnectDB(c)
     return True
 
+
+# Edit Post
 def edit_post(article, post_id):
     c = connectDB()
     post_id = str(post_id)
-    executeDB(c,"update posts set article=%s where post_id="+post_id,(article,))
+    executeDB(c, "update posts set article=%s where post_id="+post_id, (article,))
     disconnectDB(c)
     return True
 
 
+# Post List
 def post_list():
     c = connectDB()
-    result = queryDB(c,"select * from posts")
+    result = queryDB(c, "select * from posts")
     disconnectDB(c)
     return result
 
+
+# Post list Of Followers
 def post_list_by_id(user_id):
     c = connectDB()
     user_id = str(user_id)
-    result = queryDB(c,"select * from posts where user_id in (select to_id from follows where from_id="+user_id+")")
+    result = queryDB(c, "select * from posts where user_id in (select to_id from follows where from_id="+user_id+")")
     disconnectDB(c)
     return result
 
 
+# Post List Of Current User ID
 def post_list_by_my_id(user_id):
     c = connectDB()
     user_id = str(user_id)
-    result = queryDB(c,"select * from posts where user_id ="+user_id)
+    result = queryDB(c, "select * from posts where user_id ="+user_id)
     disconnectDB(c)
     return result
 
 
+# Post List By Post ID
 def post_list_by_post_id(post_id):
     c = connectDB()
     post_id = str(post_id)
-    result = queryDB(c,"select * from posts where post_id ="+post_id)
+    result = queryDB(c, "select * from posts where post_id ="+post_id)
     disconnectDB(c)
     return result
 
 
-#Like, Unlike and Like List
-def like_post(user_id,post_id):
+# Like, Unlike and Like List
+# Like Post
+def like_post(user_id, post_id):
     c = connectDB()
     user_id, post_id = str(user_id), str(post_id)
-    executeDB(c,"insert into likes values("+user_id+", "+post_id+")",())
+    executeDB(c, "insert into likes values("+user_id+", "+post_id+")", ())
     disconnectDB(c)
     return True
 
-def unlike_post(user_id,post_id):
+
+# Unlike Post
+def unlike_post(user_id, post_id):
     c = connectDB()
     user_id, post_id = str(user_id), str(post_id)
-    executeDB(c,"delete from likes where user_id="+user_id+" and post_id="+post_id,())
+    executeDB(c, "delete from likes where user_id="+user_id+" and post_id="+post_id, ())
     disconnectDB(c)
     return True
 
+
+# Like List
 def like_list_by_post_id(post_id):
     c = connectDB()
     post_id = str(post_id)
-    result = queryDB(c,"select user_id from likes where post_id = "+post_id)
+    result = queryDB(c, "select user_id from likes where post_id = "+post_id)
     result1 = [x[0] for x in result]
     disconnectDB(c)
     return result1
 
 
-#Follow, Unfollow and Follower/Following List
-def follow_user(from_id,to_id):
+# Follow, Unfollow and Follower/Following List
+# Follow User
+def follow_user(from_id, to_id):
     c = connectDB()
     from_id, to_id = str(from_id), str(to_id)
-    executeDB(c,"insert into follows values("+from_id+", "+to_id+")",())
+    executeDB(c, "insert into follows values("+from_id+", "+to_id+")", ())
     disconnectDB(c)
     return True
 
-def unfollow_user(from_id,to_id):
+
+# Unfollow User
+def unfollow_user(from_id, to_id):
     c = connectDB()
     from_id, to_id = str(from_id), str(to_id)
-    executeDB(c,"delete from follows where from_id="+from_id+" and to_id="+to_id,())
+    executeDB(c, "delete from follows where from_id="+from_id+" and to_id="+to_id, ())
     disconnectDB(c)
     return True
 
+
+# Follower List
 def follower_list(user_id):
     c = connectDB()
     user_id = str(user_id)
-    result = queryDB(c,"select username from members where user_id in (select from_id from follows where to_id="+user_id+")")
+    result = queryDB(c, "select username from members where user_id in (select from_id from follows where to_id="+user_id+")")
     result1 = [x[0] for x in result]
     disconnectDB(c)
     return result1
 
+
+# Following List
 def following_list(user_id):
     c = connectDB()
     user_id = str(user_id)
-    result = queryDB(c,"select username from members where user_id in (select to_id from follows where from_id="+user_id+")")
+    result = queryDB(c, "select username from members where user_id in (select to_id from follows where from_id="+user_id+")")
     result1 = [x[0] for x in result]
     disconnectDB(c)
     return result1
 
-#members info
+
+# Members info
 def member_info(user_id):
     c = connectDB()
     user_id = str(user_id)
-    result = queryDB(c,"select * from members where user_id="+user_id)
+    result = queryDB(c, "select * from members where user_id="+user_id)
     disconnectDB(c)
     return result
 
+
+# Fetch All Username
 def username_fetch():
     c = connectDB()
-    result = queryDB(c,"select username from members")
+    result = queryDB(c, "select username from members")
     result1 = [x[0] for x in result]
     disconnectDB(c)
     return result1
 
+
+# Fetch Username Except Input
 def username_not_mine_fetch(username):
     c = connectDB()
-    result = queryDB(c,"select username from members where username!='"+username+"'")
+    result = queryDB(c, "select username from members where username!='"+username+"'")
     result1 = [x[0] for x in result]
     disconnectDB(c)
     return result1
 
+
+# Fetch Username by User ID
 def username_fetch_by_id(user_id):
     c = connectDB()
     user_id = str(user_id)
-    result = queryDB(c,"select username from members where user_id = "+user_id)
+    result = queryDB(c, "select username from members where user_id = "+user_id)
     result = result[0][0]
     disconnectDB(c)
     return result
 
+
+# Fetch User ID By Username
 def user_id_fetch_by_name(username):
     c = connectDB()
     username = str(username)
-    result = queryDB(c,"select user_id from members where username = '"+username+"'")
+    result = queryDB(c, "select user_id from members where username = '"+username+"'")
     result = result[0][0]
     disconnectDB(c)
     return result
 
+
+# Fetch All Emails
 def email_fetch():
     c = connectDB()
-    result = queryDB(c,"select email from members")
+    result = queryDB(c, "select email from members")
     disconnectDB(c)
     return result
 
+
+# Fetch All Emails Except Input
 def email_not_mine_fetch(email):
     c = connectDB()
-    result = queryDB(c,"select email from members where email!='"+email+"'")
+    result = queryDB(c, "select email from members where email!='"+email+"'")
     result1 = [x[0] for x in result]
     disconnectDB(c)
     return result1
 
+
+# List Members
 def members_list():
     c = connectDB()
-    result = queryDB(c,"select username, email, dob, bio from members")
+    result = queryDB(c, "select username, email, dob, bio from members")
     disconnectDB(c)
     return result
 
-#Flush tables
+
+# Flush tables
 def flush_posts(user_id):
     c = connectDB()
     user_id = str(user_id)
@@ -243,21 +305,25 @@ def flush_posts(user_id):
     post_list = [x[0] for x in result]
     for post in post_list:
         flush_likes(post)
-    executeDB(c,"delete from posts where user_id="+user_id,())
+    executeDB(c, "delete from posts where user_id="+user_id, ())
     disconnectDB(c)
     return True
 
+
+# Flush Follows
 def flush_follow(user_id):
     c = connectDB()
     user_id = str(user_id)
-    executeDB(c,"delete from follows where from_id="+user_id+" or to_id="+user_id,())
+    executeDB(c, "delete from follows where from_id="+user_id+" or to_id="+user_id, ())
     disconnectDB(c)
     return True
 
+
+# Flush Likes
 def flush_likes(post_id):
     c = connectDB()
     post_id = str(post_id)
-    executeDB(c,"delete from likes where post_id="+post_id,())
+    executeDB(c, "delete from likes where post_id="+post_id, ())
     disconnectDB(c)
     return True
 
